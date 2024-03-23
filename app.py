901cf869550b3bb67667d50ccb2106f955a1002e
Ozan UYSAL <ozan.uysal@msn.com>
2024-03-23 22:44:09 +0300

    examples added

--- app.py
+++ app.py
@@ -2,21 +2,23 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-cnst_pipeline = "Pipeline"
-crd_name_list = [ cnst_pipeline ]
+cnst_pipeline = "PipelineRun"
+cnst_notebook = "Notebook"
+crd_name_list = [ cnst_pipeline, cnst_notebook ]
 
 @app.route('/mutate', methods=['POST'])
 def mutate_pod():
-    print(request_data)
-    if(request_data["name"] not in  crd_name_list): return request.json
+    print(request.json)
+    #For Emergency. By pass everything
+    #return request.json
+    #
+
+    jsn = request.json
+    if(jsn["kind"] not in  crd_name_list): return jsn
 
     #description'dan hash bilgileri (#crmapdb) alınıp, bir de cpu/ram alınırsa,  
 
-    request_data = request.json
-    
-    
-
-    return request_data
+    return jsn
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
