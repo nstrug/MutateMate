d585f73f2f45b37fbae73984a35d5f35d6c5db6e
Ozan UYSAL <ozan.uysal@msn.com>
2024-03-24 01:31:04 +0300

    dummy data added

--- services/KubeWrapperService.py
+++ services/KubeWrapperService.py
@@ -9,19 +9,19 @@ class KubeWrapperService:
         
         pass
 
-    # CPU, RAM, GPU all values related part:
-
     def gather_all_resources(self, url, access_token):
         pass
 
+    # CPU, RAM, GPU all values related part:
+    
     def get_cpu_value(self) -> str:
-        pass
+        return "1"
 
     def get_ram_value(self) -> str:
-        pass
+        return "1G"
 
     def get_gpu_value(self) -> str:
-        pass
+        return "1"
 
     def get_all_resources(self) -> str:
         return self.get_cpu_value(), self.get_ram_value(), self.get_gpu_value()
@@ -31,10 +31,25 @@ class KubeWrapperService:
     def get_all_secrets(self) -> dict:
         #keys are names of secret
         #values are all key/value pairs inside that secret
-        pass
+        tmp = {
+            "secret1":{
+                "secret1user":"user1",
+                "secret1pass":"pass1"
+            },
+            "secret2":{
+                "secret2user":"user2",
+                "secret2pass":"pass2"
+            },
+            "secret3":{
+                "secret3user":"user3",
+                "secret3pass":"pass3"
+            }
+        }
+
+        return tmp
 
     def get_namespace_definition(self, name_space : str) -> str:
-        pass
+        return "This is a project description #secret1 #secret2"
 
     def get_namespace_definition_hashtags(self, name_space : str) -> list:
         tmp_desc = self.get_namespace_definition(name_space)
@@ -45,7 +60,7 @@ class KubeWrapperService:
         for itm in allowed_names:
             if(itm in whole_secret):
                 vals = whole_secret[itm]
-                for key, val in vals:
+                for key, val in vals.items():
                     tmp[key] = val
 
         return tmp
--- services/PipelineRunMutaterService.py
+++ services/PipelineRunMutaterService.py
@@ -7,4 +7,17 @@ class PipelineRunMutaterService:
         pass
 
     def mutate(self, data : json, secrets : dict, cpu : str, ram : str, gpu : str) -> json:
-        pass
+        data = self.assign_secrets(data, secrets)
+
+        return data
+
+    def assign_secrets(self, data_json : json, secrets : dict) -> json:
+        tmp_tasks = data_json["spec"]["pipelineSpec"]["task"]
+
+        for itm_task in tmp_tasks:
+            tmp_env = itm_task["taskSpec"]["steps"]["0"]["env"]
+
+            for secret_key, secret_value in secrets.items():
+                tmp_env.append(secret_key, secret_value)
+        
+        return data_json
