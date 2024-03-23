97246e4b883823d02e2c7de98b790bff9ebe61d1
Ozan UYSAL <ozan.uysal@msn.com>
2024-03-24 02:13:31 +0300

    Pipeline Mutater secret assignation tested

--- services/PipelineRunMutaterService.py
+++ services/PipelineRunMutaterService.py
@@ -7,17 +7,18 @@ class PipelineRunMutaterService:
         pass
 
     def mutate(self, data : json, secrets : dict, cpu : str, ram : str, gpu : str) -> json:
+        data["metadata"]["labels"]["MutatingMate"] = "PipelineRunMutater"
         data = self.assign_secrets(data, secrets)
 
         return data
 
     def assign_secrets(self, data_json : json, secrets : dict) -> json:
-        tmp_tasks = data_json["spec"]["pipelineSpec"]["task"]
+        tmp_tasks = data_json["spec"]["pipelineSpec"]["tasks"]
 
         for itm_task in tmp_tasks:
-            tmp_env = itm_task["taskSpec"]["steps"]["0"]["env"]
+            tmp_env = itm_task["taskSpec"]["steps"][0]["env"]            
 
             for secret_key, secret_value in secrets.items():
-                tmp_env.append(secret_key, secret_value)
+                tmp_env.append({ "name":secret_key, "value": secret_value })
         
         return data_json
