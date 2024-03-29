06f314a7915443331e80cf069cbeec3d7d9d7a23
Ozan UYSAL <ozan.uysal@msn.com>
2024-03-29 19:06:53 +0300

    json serilization added for jsonbag class

--- src/services/JsonBag.py
+++ src/services/JsonBag.py
@@ -5,6 +5,15 @@ import json
 class JsonBag():
     def __init__(self, json_data : json) -> None:
         self.assign_values(json_data)
+        pass
+    
+    def to_json(self):
+        return {
+            'kind': self.kind,
+            'name': self.name,
+            "namespace": self.namespace,
+            "workbench_description": self.workbench_description
+        }
 
     def assign_values(self, json_data):
         tmp_obj = json_data["request"]["object"]
@@ -12,4 +21,4 @@ class JsonBag():
         self.kind = tmp_obj["kind"]
         self.name = tmp_obj["metadata"]["name"]
         self.namespace = tmp_obj["metadata"]["namespace"]
-        self.workbench_description = tmp_obj["metadata"]["annotations"]["openshift.io/description"]
+        self.workbench_description = tmp_obj["metadata"]["annotations"].get("openshift.io/description")
