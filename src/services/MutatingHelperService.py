34aa186b0f8cc3937e223046efd39628067816d9
Ozan UYSAL <ozan.uysal@msn.com>
2024-04-03 12:07:41 +0300

    pipeline resource assignment theoric implementation 11

--- src/services/MutatingHelperService.py
+++ src/services/MutatingHelperService.py
@@ -11,16 +11,7 @@ class MutatingHelperService:
     
     def add_resource_for_pipeline(self, task_index : int, cpu_limit : str, ram_limit : str, gpu_limit : str):
         return {
-            "op": "replace", "path": f"/spec/pipelineSpec/tasks/{task_index}/taskSpec/steps/0/" , 
-                "value": {
-                    "resources": { "limit" : { "memory": "\"1Gi\"" } 
-                                }
-                }
-            }
-        
-        return {
-            "op": "add", "path": f"/spec/pipelineSpec/tasks/{task_index}/taskSpec/steps/0/resources/" , 
-                "value": { "limits": { "nvidia.com/gpu": gpu_limit },  
-                "requests": { "cpu": cpu_limit, "memory": ram_limit }
+            "op": "add", "path": f"/spec/pipelineSpec/tasks/{task_index}/taskSpec/steps/0/resources" , 
+                "value": { "limits": { "nvidia.com/gpu": gpu_limit, "cpu": cpu_limit, "memory": ram_limit }
             }
         }
