f1440ecdcd92f599933eae0f7a3a1a299ecdf07b
Ozan UYSAL <ozan.uysal@msn.com>
2024-04-03 21:03:06 +0300

    pipeline gpu request limit added

--- src/services/MutatingHelperService.py
+++ src/services/MutatingHelperService.py
@@ -12,29 +12,16 @@ class MutatingHelperService:
     def add_secret_for_pipeline(self, task_index : int, key_name : str, real_value : str):
         return {"op": "add", "path": f"/spec/pipelineSpec/tasks/{task_index}/taskSpec/steps/0/env/-", "value": {"name": key_name, "value": real_value}}
     
-    def add_resource_for_pipeline(self, task_index : int, cpu_limit : str, ram_limit : str, gpu_limit : str) -> []:
-        payload = []
-        #this is for real thing we want:
-        payload.append(
-        {
+    def add_resource_for_pipeline(self, task_index : int, cpu_limit : str, ram_limit : str, gpu_limit : str):
+        min_gpu = int(gpu_limit)
+        if(min_gpu > 1): min_gpu = 1
+        return {
             "op": "add", "path": f"/spec/pipelineSpec/tasks/{task_index}/taskSpec/steps/0/computeResources" , 
                 "value": { 
                     "limits": { "nvidia.com/gpu": gpu_limit, "cpu": cpu_limit, "memory": ram_limit },
-                    "requests": { "cpu": "1m", "memory": "1Mi" }
+                    "requests": { "nvidia.com/gpu": min_gpu, "cpu": "1m", "memory": "1Mi" }
             }
-        })
-
-        # #this thing is a workaround. We don't want to assing request but it is default setting
-        # min_gpu = gpu_limit
-        # if( int(min_gpu) > 0): min_gpu = 1
-        # payload.append(
-        # {
-        #     "op": "add", "path": f"/spec/pipelineSpec/tasks/{task_index}/taskSpec/steps/0/computeResources" , 
-        #         "value": {  }
-        #     }
-        # }) 
-
-        return payload
+        }
     
     def add_our_label(self, old_labels : dict):
         #[{"op": "add", "path": "/metadata/labels", "value": {"thy.editedby": "MutateMate" }}]
